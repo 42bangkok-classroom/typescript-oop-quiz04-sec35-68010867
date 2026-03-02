aa6bacc10acef977a05e4adde5d9676d354e4e3d
Phasawut Imsurman <68010867@kmitl.ac.th>
2026-03-02 10:20:02 +0700

    88

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller()
+@Controller('users')
 export class UserController {
   constructor(private readonly appService: UserService) {}
 
-  @Get()
+  @Get('test')
   test(): string[] {
     return this.appService.test();
   }
